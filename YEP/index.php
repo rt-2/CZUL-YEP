a2fd419b126345a8de5f0f1a452192beee3fdc1d
Yanick <rt-2@rt-2.net>
2019-11-06 15:51:32 -0500

    Update index.php

--- YEP/index.php
+++ YEP/index.php
@@ -6,8 +6,8 @@
 ?>
 <html>
 <head>
-	<meta charset="UTF-8">
-    	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta charset="UTF-8" />
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>YEP - Yanick's Euroscope ᶠᵃᵏᵉPlugin</title>
 
 	<script>
