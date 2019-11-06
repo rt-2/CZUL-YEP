e45038b8a4cf67042e7f4253e9ccd02934fcb78a
rt-2 <rt-2@rt-2.net>
2019-11-06 15:47:14 -0500

    Update cyqb.php

--- YEP/0.1/cyqb.php
+++ YEP/0.1/cyqb.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-	<html>
+<html>
 	<head>
 		<meta charset="UTF-8">
-		<title>YEP (Yanick's Euroscope Program/Plugin</title>
-		
-		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		
-		<style>
+			<title>YEP (Yanick's Euroscope Program/Plugin</title>
+
+
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"/>
+
+			<style>
 			* {
 				margin: 0 0;
 				padding: 0 0;
@@ -22,44 +22,44 @@
 				height:100%;width:100%;
 				top: 0; bottom: 0; left: 0; right: 0;
 			}
-		</style>
-		
-		<script>
-			
+			</style>
+
+			<script>
+
 			/*
 				jQuery Document Ready
 			*/
 			$( document ).ready(function() {
 				// Document ready
-				
+
 				setInterval(function () {
 					document.getElementById('WeatherIframe').contentWindow.location.reload();
-					
+
 				}, 600000);
-				
+
 			});
-			
+
 			/*
 				Javascript Try/Catch
 			* /
 			try {
 				// Try something
-				
+
 			}
 			catch(err) {
 				// Catch errors (Should be err.message)
 				if(console && console.log) console.log(err);
 				if(console && console.log) console.log("ERROR:\nAt column "+err.columnNumber+" on line "+err.lineNumber+" of file:"+err.fileName+"\nMessage: "+err.message+"\nStack:\n"+err.stack);
 			}
-			
+
 			/*
 				jQuery Asynchronous calls (AJAX)
 			* /
 			function sendExampleAJAX() {
-				
+
 				fd = new FormData();
 				fd.append( 'file', $('#input1').val() );
-				
+
 				$.ajax({
 					url : "AJAX_POST_URL",
 					type: "POST",
@@ -72,43 +72,44 @@
 					},
 					error: function (responseObject, status, error)
 					{
-				 
+
 					}
 				});
 			}
 			*/
-		</script>
+			</script>
 
-	</head>
+		</head>
 
 		<body>
-		<table id="theTable">
-			<tr>
-				<td colspan="2" height="40%" width="100%">
-					<iframe src="http://rt-2.net/YEP/CaNotAPI/cyqb_app.php">
-				
-					</iframe>
+			<table id="theTable">
+				<tr>
+					<td colspan="2" height="40%" width="100%">
+						<iframe src="http://rt-2.net/YEP/CaNotAPI/cyqb_app.php">
 
-				</td>
+						</iframe>
 
-			</tr>
-			<tr>
-				<td>
-					<iframe id="WeatherIframe" src="http://rt-2.net/YEP/Weather/?icao=cyqb">
-				
-					</iframe>
+					</td>
 
-				</td>
-				<td width="30%">
-					<iframe src="http://rt-2.net/YEP/WLTs/">
-				
-					</iframe>
+				</tr>
+				<tr>
+					<td>
+						<iframe id="WeatherIframe" src="http://rt-2.net/YEP/Weather/?icao=cyqb">
 
-				</td>
-			</tr>
-		</table>
+						</iframe>
 
-		
+					</td>
+					<td width="30%">
+						<iframe src="http://rt-2.net/YEP/WLTs/">
+
+						</iframe>
+
+					</td>
+				</tr>
+			</table>
+
+
 		</body>
 
 	</html>
+	
