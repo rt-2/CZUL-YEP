5c38bf73f3511c186813149cb13033c7679919c2
Yanick <rt-2@rt-2.net>
2019-11-06 15:49:07 -0500

    Update cyqb.php
    indent

--- YEP/0.1/cyqb.php
+++ YEP/0.1/cyqb.php
@@ -8,36 +8,36 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"/>
 
 	<style>
-	* {
-		margin: 0 0;
-		padding: 0 0;
-		box-sizing: border-box;
-	}
-	iframe {
-		width: 100%;
-		height: 100%;
-	}
-	#theTable {
-		position: absolute;
-		height:100%;width:100%;
-		top: 0; bottom: 0; left: 0; right: 0;
-	}
+		* {
+			margin: 0 0;
+			padding: 0 0;
+			box-sizing: border-box;
+		}
+		iframe {
+			width: 100%;
+			height: 100%;
+		}
+		#theTable {
+			position: absolute;
+			height:100%;width:100%;
+			top: 0; bottom: 0; left: 0; right: 0;
+		}
 	</style>
 
 	<script>
 
-	/*
-		jQuery Document Ready
-	*/
-	$( document ).ready(function() {
-		// Document ready
+		/*
+			jQuery Document Ready
+		*/
+		$( document ).ready(function() {
+			// Document ready
 
-		setInterval(function () {
-			document.getElementById('WeatherIframe').contentWindow.location.reload();
+			setInterval(function () {
+				document.getElementById('WeatherIframe').contentWindow.location.reload();
 
-		}, 600000);
+			}, 600000);
 
-	});
+		});
 
 	</script>
 
