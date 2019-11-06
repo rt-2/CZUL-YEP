ab1d15c5cb001ca7dde64ce0733ae840c8d0bd34
rt-2 <rt-2@rt-2.net>
2019-11-06 15:48:18 -0500

    indent

--- YEP/0.1/cyqb.php
+++ YEP/0.1/cyqb.php
@@ -1,115 +1,77 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-			<title>YEP (Yanick's Euroscope Program/Plugin</title>
+<head>
+	<meta charset="UTF-8">
+	<title>YEP (Yanick's Euroscope Program/Plugin</title>
 
 
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"/>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"/>
 
-			<style>
-			* {
-				margin: 0 0;
-				padding: 0 0;
-				box-sizing: border-box;
-			}
-			iframe {
-				width: 100%;
-				height: 100%;
-			}
-			#theTable {
-				position: absolute;
-				height:100%;width:100%;
-				top: 0; bottom: 0; left: 0; right: 0;
-			}
-			</style>
+	<style>
+	* {
+		margin: 0 0;
+		padding: 0 0;
+		box-sizing: border-box;
+	}
+	iframe {
+		width: 100%;
+		height: 100%;
+	}
+	#theTable {
+		position: absolute;
+		height:100%;width:100%;
+		top: 0; bottom: 0; left: 0; right: 0;
+	}
+	</style>
 
-			<script>
+	<script>
 
-			/*
-				jQuery Document Ready
-			*/
-			$( document ).ready(function() {
-				// Document ready
+	/*
+		jQuery Document Ready
+	*/
+	$( document ).ready(function() {
+		// Document ready
 
-				setInterval(function () {
-					document.getElementById('WeatherIframe').contentWindow.location.reload();
+		setInterval(function () {
+			document.getElementById('WeatherIframe').contentWindow.location.reload();
 
-				}, 600000);
+		}, 600000);
 
-			});
+	});
 
-			/*
-				Javascript Try/Catch
-			* /
-			try {
-				// Try something
+	</script>
 
-			}
-			catch(err) {
-				// Catch errors (Should be err.message)
-				if(console && console.log) console.log(err);
-				if(console && console.log) console.log("ERROR:\nAt column "+err.columnNumber+" on line "+err.lineNumber+" of file:"+err.fileName+"\nMessage: "+err.message+"\nStack:\n"+err.stack);
-			}
+</head>
 
-			/*
-				jQuery Asynchronous calls (AJAX)
-			* /
-			function sendExampleAJAX() {
+<body>
+	<table id="theTable">
+		<tr>
+			<td colspan="2" height="40%" width="100%">
+				<iframe src="http://rt-2.net/YEP/CaNotAPI/cyqb_app.php">
 
-				fd = new FormData();
-				fd.append( 'file', $('#input1').val() );
+				</iframe>
 
-				$.ajax({
-					url : "AJAX_POST_URL",
-					type: "POST",
-					data: fd,
-					  processData: false,
-					  contentType: false,
-					success: function(data, status, responseObject)
-					{
-						//data - response from server
-					},
-					error: function (responseObject, status, error)
-					{
+			</td>
 
-					}
-				});
-			}
-			*/
-			</script>
+		</tr>
+		<tr>
+			<td>
+				<iframe id="WeatherIframe" src="http://rt-2.net/YEP/Weather/?icao=cyqb">
 
-		</head>
+				</iframe>
 
-		<body>
-			<table id="theTable">
-				<tr>
-					<td colspan="2" height="40%" width="100%">
-						<iframe src="http://rt-2.net/YEP/CaNotAPI/cyqb_app.php">
+			</td>
+			<td width="30%">
+				<iframe src="http://rt-2.net/YEP/WLTs/">
 
-						</iframe>
+				</iframe>
 
-					</td>
-
-				</tr>
-				<tr>
-					<td>
-						<iframe id="WeatherIframe" src="http://rt-2.net/YEP/Weather/?icao=cyqb">
-
-						</iframe>
-
-					</td>
-					<td width="30%">
-						<iframe src="http://rt-2.net/YEP/WLTs/">
-
-						</iframe>
-
-					</td>
-				</tr>
-			</table>
+			</td>
+		</tr>
+	</table>
 
 
-		</body>
+</body>
 
-	</html>
+</html>
 	
